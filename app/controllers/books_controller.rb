4baef47a0a59730e145ce3eebe15e04e49622203
gnahidi <nahidi_ghazaleh@bah.com>
2014-09-04 21:18:02 -0400

    renamed the functions file

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,13 @@
 class BooksController < ApplicationController
+
 def index
-
-
-@available_at = Time.now
-@books = Book.all
-
+	@available_at = Time.now
+	@books = Book.all
 end
+
+
+def show
+  	@book = Book.find(params[:id])
+end
+
 end
