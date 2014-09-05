24f5ba04fdc2096fb8fbc4437f69e251bbcb46cb
gnahidi <nahidi_ghazaleh@bah.com>
2014-09-04 20:59:15 -0400

    renamed the functions file

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,9 @@
 class BooksController < ApplicationController
 def index
-	
+
+
 @available_at = Time.now
-@books = ["Atlas Shrugged", "Ender's Game", "Hamlet", "The Hobbit"]
+@books = Book.all
 
 end
 end
