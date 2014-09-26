b74eb50d8803b9e5b57c79a4840fc36d282eb752
gnahidi <nahidi_ghazaleh@bah.com>
2014-09-25 20:29:55 -0400

    Class 04 - Lab5

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Bookstore::Application.routes.draw do
   
   # Begin Partials Implementation
   resources :books
+  root 'books#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
